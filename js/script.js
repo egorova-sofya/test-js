a2a8ccb329c27ca360411259b08c87a44dbdf58d
egorova-sofya <EgorovaSjob@yandex.ru>
2021-03-04 20:50:22 +0300

    начала создавать приложение

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 "use sctrict";
 
-const category = "toys";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(`https://someurl.com/${category}/5`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const user = "Ivan";
-alert(`Hello, ${user}`);
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?");
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
