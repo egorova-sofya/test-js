cf8763284d3e4a2b6f5353d5cd367b3c8292b876
egorova-sofya <EgorovaSjob@yandex.ru>
2021-03-06 15:00:29 +0300

    Добавила условия и циклы

--- js/script.js
+++ js/script.js
@@ -11,12 +11,35 @@ const personalMovieDB = {
 };
 
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+for (let i = 0; i < 2; i++) {
 const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?"),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?");
+      b = prompt("На сколько оцените его?");
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+    if (a != null && b!= null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("error");
+        i--;
+    }
 
-    console.log(personalMovieDB);
+   
+    
+}
+
+console.log(personalMovieDB);
+
+
+
+
+
